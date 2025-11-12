263c8f3ba5d2d68f434fed91171dff2f36a41e77
Mayank <mayank@localhost.localdomain>
2025-11-12 19:15:06 +0600

    updated code

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+dotenv.config();
+// packages
+import path from "path";
+import express from "express";
+import cookieParser from "cookie-parser";
+
+// Utiles
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
+
+connectDB()
+    .then(() => {
+        app.listen(8000 || process.env.PORT , () => {
+            console.log(`Server is running on PORT: 8000`)
+        })
+    })
+    .catch(error => {
+        console.log(`mongoDB connection failed: ${error}`);
+    })
